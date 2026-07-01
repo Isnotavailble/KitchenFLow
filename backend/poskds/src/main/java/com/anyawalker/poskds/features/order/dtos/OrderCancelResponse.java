656d29785393e3604f66850a31b0c08a8807cfc6
Anakin <kaungkaung272005@gmail.com>
2026-07-01 21:19:50 +0630

    updated: db migration with minor adjustment  ( user,token pk id is biginit or Long for Spring security )

--- backend/poskds/src/main/java/com/anyawalker/poskds/features/order/dtos/OrderCancelResponse.java
+++
@@ -1,6 +0,0 @@
-package com.anyawalker.poskds.features.order.dtos;
-
-import java.time.LocalDateTime;
-
-public record OrderCancelResponse(Long orderId, Long userId, String status, LocalDateTime resolvedAt) {
-}
